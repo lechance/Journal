87e41c1f239eca855a2248b420a55f4bf619cb46
Lechance <lechance@sina.com>
2016-05-11 21:45:23 +0800

    refactor with gradle

---
+++ app/src/main/java/com/zhangzhiqiang/android/journal/model/Journal.java
@@ -0,0 +1,47 @@
+package com.zhangzhiqiang.android.journal.model;
+
+import java.io.Serializable;
+
+public class Journal implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    int id;
+    String title;
+    String content;
+    String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
